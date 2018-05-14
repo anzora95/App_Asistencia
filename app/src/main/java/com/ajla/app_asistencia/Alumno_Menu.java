3cf084ca15a0282dabfdd626fc9052c3fe6206f3
Unknown <josephe95@hotmail.com>
2018-05-14 12:36:45 -0600

    Creacion de pantallas v1
    Se crearon pantallas de login y pantallas de menu alumno y de menu materias a la cual falta modificar

---
+++ app/src/main/java/com/ajla/app_asistencia/Alumno_Menu.java
@@ -0,0 +1,13 @@
+package com.ajla.app_asistencia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Alumno_Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alumno__menu);
+    }
+}
