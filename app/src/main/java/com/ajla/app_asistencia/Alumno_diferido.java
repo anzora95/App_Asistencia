90b0ea0a3b23a08d946fa448d598a582f3632a84
Unknown <josephe95@hotmail.com>
2018-05-15 15:20:01 -0600

    Vistas terminadas v1
    Alumno asistencia se unio en una sola vista

---
+++ app/src/main/java/com/ajla/app_asistencia/Alumno_diferido.java
@@ -0,0 +1,13 @@
+package com.ajla.app_asistencia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Alumno_diferido extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alumno_diferido);
+    }
+}
--- app/src/main/java/com/ajla/app_asistencia/MainActivity.java
+++ app/src/main/java/com/ajla/app_asistencia/MainActivity.java
@@ -1,13 +1,28 @@
 package com.ajla.app_asistencia;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_login= findViewById(R.id.btn_login);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               Intent inte_inicio = new Intent(MainActivity.this,Admin_Inicio.class);
+               startActivity(inte_inicio);
+            }
+        });
     }
 }
