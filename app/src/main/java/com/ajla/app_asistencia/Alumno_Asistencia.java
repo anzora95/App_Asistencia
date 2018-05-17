94c5a2be18c7f3aa74a1b797aca6397c64af6f95
Unknown <josephe95@hotmail.com>
2018-05-17 02:19:01 -0600

    Pantalla Asistencia
    La pantalla asistencia esta funcionando y solo espera datos de la base de datos

--- app/src/main/java/com/ajla/app_asistencia/Alumno_Asistencia.java
+++ app/src/main/java/com/ajla/app_asistencia/Alumno_Asistencia.java
@@ -2,12 +2,142 @@ package com.ajla.app_asistencia;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class Alumno_Asistencia extends AppCompatActivity {
+
+    Spinner sp_asis;
+    ListView lv_asis;
+    ArrayList ls;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alumno__asistencia);
+
+        sp_asis=findViewById(R.id.spi_asis);
+        lv_asis=findViewById(R.id.lv_asis);
+
+        //lista spinner
+        ls=new ArrayList();
+        for(int i=0;i<=4;i++){
+
+            ls.add("Materia"+i);
+        }
+
+        //---------------------------------------------------------------
+        //LIstas para listview
+
+
+
+        //Array Adapter para spinner
+
+        ArrayAdapter<CharSequence> adaptersp = new ArrayAdapter(Alumno_Asistencia.this,android.R.layout.simple_spinner_item,ls);
+        sp_asis.setAdapter(adaptersp);
+
+
+
+        sp_asis.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+
+                int posicion =adapterView.getSelectedItemPosition();
+                Toast.makeText(Alumno_Asistencia.this,"la posicion es: "+posicion,Toast.LENGTH_LONG).show();
+
+                if(posicion==0){
+                    //Lista listview1
+                    ArrayList lsv=new ArrayList();
+
+                    for (int j=0;j<=36;j++){
+
+                        lsv.add("Semana "+(j+1));
+
+                    }
+
+                    //Arrayadapter para listview
+
+                    ArrayAdapter<String> adapterlv = new ArrayAdapter(Alumno_Asistencia.this,android.R.layout.simple_list_item_1,lsv);
+                    lv_asis.setAdapter(adapterlv);
+                }
+
+                else if(posicion==1){
+                    //Lista listview1
+                    ArrayList lsv=new ArrayList();
+
+                    for (int j=0;j<=36;j++){
+
+                        lsv.add("clase "+(j+1));
+
+                    }
+                    //Arrayadapter para listview
+
+                    ArrayAdapter<String> adapterlv = new ArrayAdapter(Alumno_Asistencia.this,android.R.layout.simple_list_item_1,lsv);
+                    lv_asis.setAdapter(adapterlv);
+                }
+
+                else if(posicion==2){
+                    //Lista listview1
+                    ArrayList lsv=new ArrayList();
+
+                    for (int j=0;j<=36;j++){
+
+                        lsv.add("Evaluacion "+(j+1));
+
+                    }
+                    //Arrayadapter para listview
+
+                    ArrayAdapter<String> adapterlv = new ArrayAdapter(Alumno_Asistencia.this,android.R.layout.simple_list_item_1,lsv);
+                    lv_asis.setAdapter(adapterlv);
+                }
+                else if(posicion==3){
+                    //Lista listview1
+                    ArrayList lsv=new ArrayList();
+
+                    for (int j=0;j<=36;j++){
+
+                        lsv.add("Periodo "+(j+1));
+
+                    }
+                    //Arrayadapter para listview
+
+                    ArrayAdapter<String> adapterlv = new ArrayAdapter(Alumno_Asistencia.this,android.R.layout.simple_list_item_1,lsv);
+                    lv_asis.setAdapter(adapterlv);
+                }
+                else if(posicion == 4){
+                    //Lista listview1
+                    ArrayList lsv=new ArrayList();
+
+                    for (int j=0;j<=36;j++){
+
+                        lsv.add("Pokemon "+(j+1));
+
+                    }
+                    //Arrayadapter para listview
+
+                    ArrayAdapter<String> adapterlv = new ArrayAdapter(Alumno_Asistencia.this,android.R.layout.simple_list_item_1,lsv);
+                    lv_asis.setAdapter(adapterlv);
+                }
+
+
+
+
+
+
+
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/ajla/app_asistencia/Alumno_diferido.java
+++ app/src/main/java/com/ajla/app_asistencia/Alumno_diferido.java
@@ -3,11 +3,16 @@ package com.ajla.app_asistencia;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class Alumno_diferido extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alumno_diferido);
+
+
     }
 }
