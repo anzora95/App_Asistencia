1cea3cbe890bdca412c2bdd077a9c7aacb0eb66c
Unknown <gloria.ganuza@hotmail.com>
2018-06-15 11:38:32 -0600

    Web Service
    servicio actualizar

--- app/src/main/java/com/ajla/app_asistencia/jefeDiferido.java
+++ app/src/main/java/com/ajla/app_asistencia/jefeDiferido.java
@@ -5,6 +5,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,11 +14,33 @@ import android.widget.Toast;
 
 import com.ajla.app_asistencia.Entidades.Alumno;
 import com.ajla.app_asistencia.Entidades.Ciclo;
+import com.ajla.app_asistencia.Entidades.VolleySingleton;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
-public class jefeDiferido extends AppCompatActivity implements View.OnClickListener {
+import org.apache.http.conn.ssl.StrictHostnameVerifier;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class jefeDiferido extends AppCompatActivity implements Response.Listener<JSONObject>, Response.ErrorListener {
     EditText solicitud,nombre,apellido,fecha;
-    String codigo,nombres,apellidos;
-    ConexionSQLiteHelper conec;
+    String carnet,nombres,apellidos;
+    String codigo;
+    Button btnAceptar, btnDenegar;//No estoy segura.
+    RequestQueue request;
+    JsonObjectRequest jsonObjectRequest;
+    StringRequest stringRequest;
+  //  ConexionSQLiteHelper conec;
 
 
 
@@ -28,22 +51,42 @@ public class jefeDiferido extends AppCompatActivity implements View.OnClickListe
 
         Bundle listav = getIntent().getExtras();
         codigo = listav.getString("jefecoddife");
+        carnet = listav.getString("carnetAlumDif");
 
-        conec= new ConexionSQLiteHelper(getApplicationContext());
+        //conec= new ConexionSQLiteHelper(getApplicationContext());
 
         solicitud=(EditText) findViewById(R.id.idnombre);
         nombre=(EditText) findViewById(R.id.idMateria);
         apellido=(EditText) findViewById(R.id.idapedif);
-
+        btnAceptar=(Button) findViewById(R.id.aceptardife); //No estoy segura
+        btnDenegar=(Button) findViewById(R.id.denegardife); //No estoy segura
 
         solicitud.setText(codigo);
 
+        cargarWebService();
 
+
+
+        //EVENTOS DE LOS BOTONES --------------No estoy segura
+
+        btnAceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                webServiceActualizarAceptado();
+            }
+        });
+
+        btnDenegar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                webServiceActualizarDenegado();
+            }
+        });
+
+/*
         Alumno alu;
-        SQLiteDatabase db1= conec.getReadableDatabase();
-
-
-        Cursor cursor =db1.rawQuery("SELECT * FROM alumno inner join solicitud_diferido on alumno.carnet=solicitud_diferido.carnet where solicitud_diferido.id_solicitud == "+codigo+"",null);
+        //SQLiteDatabase db1= conec.getReadableDatabase();
+        //Cursor cursor =db1.rawQuery("SELECT * FROM alumno inner join solicitud_diferido on alumno.carnet=solicitud_diferido.carnet where solicitud_diferido.id_solicitud == "+codigo+"",null);
         while (cursor.moveToNext()) {
             alu =new Alumno();
             alu.setNom_alum(cursor.getString(1));
@@ -56,17 +99,18 @@ public class jefeDiferido extends AppCompatActivity implements View.OnClickListe
         nombre.setText(nombres);
         apellido.setText(apellidos);
 
+*/
 
 
+    }  //Fin del OnCreate
 
-    }
-
+    /*
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.aceptardife:
-                SQLiteDatabase db = conec.getWritableDatabase();
-                db.execSQL("UPDATE solicitud_diferido set estado = 1 where id_solicitud == '" + codigo+ "'");
+               // SQLiteDatabase db = conec.getWritableDatabase();
+               // db.execSQL("UPDATE solicitud_diferido set estado = 1 where id_solicitud == '" + codigo+ "'");
                 Toast.makeText(jefeDiferido.this,"se aprobo la solicitud",Toast.LENGTH_LONG).show();
                 Intent inten = new Intent (jefeDiferido.this,jefeMenu.class);
                 startActivity(inten);
@@ -78,5 +122,104 @@ public class jefeDiferido extends AppCompatActivity implements View.OnClickListe
                 startActivity(inten1);
             break;
         }
+    }*/
+
+
+    private void webServiceActualizarAceptado(){
+
+        String url="https://pdmconsumo.000webhostapp.com/ws_actualizar_estadoDiferido.php?id_solicitud="+solicitud+"&estado="+"1";
+
+
+        stringRequest=new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                if (response.trim().equalsIgnoreCase("actualiza")){
+                    // etiNombre.setText("");
+                    //  txtDocumento.setText("");
+                    //   etiProfesion.setText("");
+                    Toast.makeText(getApplicationContext(),"Se ha Actualizado con exito",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(),"No se ha Actualizado ",Toast.LENGTH_SHORT).show();
+                    Log.i("RESPUESTA: ",""+response);
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),"No se ha podido conectar",Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            protected Map<String,String> getParams() throws AuthFailureError {
+                String id_solicitud=codigo;
+                String nom_alum=nombres.toString();
+                String apel_alum=apellidos.toString();
+                String estado="1";
+
+
+
+
+                Map<String,String> parametros=new HashMap<>();
+                parametros.put("id_sol",id_solicitud);
+                parametros.put("nom_alu",nom_alum);
+                parametros.put("ape_alu",apel_alum);
+                parametros.put("est",estado);
+
+
+                return parametros;
+            }
+        };
+        //request.add(stringRequest);
+        VolleySingleton.getIntanciaVolley(getApplicationContext()).addToRequestQueue(stringRequest);
+
+
     }
+
+    private void webServiceActualizarDenegado(){
+
+    }
+
+
+
+    private void cargarWebService(){
+
+
+        String url="https://pdmconsumo.000webhostapp.com/consultarAlumno2.php?carnet=" +carnet;
+
+
+        jsonObjectRequest=new JsonObjectRequest(Request.Method.GET,url,null, this,this);
+        // request.add(jsonObjectRequest);
+        VolleySingleton.getIntanciaVolley(getApplicationContext()).addToRequestQueue(jsonObjectRequest);
+
+
+    }
+
+
+    @Override
+    public void onResponse(JSONObject response) {
+        Alumno miUsuario=new Alumno();
+
+
+        JSONArray json=response.optJSONArray("CarnetDi");
+        JSONObject jsonObject=null;
+
+        try {
+            jsonObject=json.getJSONObject(0);
+            miUsuario.setNom_alum(jsonObject.optString("NOM_ALUM"));
+            miUsuario.setApel_alum(jsonObject.optString("APEL_ALUM"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        Toast.makeText(getApplicationContext(),"No se pudo Consultar ",Toast.LENGTH_SHORT).show();
+        Log.i("ERROR",error.toString());
+    }
+
+
+
 }
--- app/src/main/java/com/ajla/app_asistencia/jefeListDiferidos.java
+++ app/src/main/java/com/ajla/app_asistencia/jefeListDiferidos.java
@@ -6,24 +6,40 @@ import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.ajla.app_asistencia.Entidades.Materia;
 import com.ajla.app_asistencia.Entidades.Solicitud_Diferido;
+import com.ajla.app_asistencia.Entidades.VolleySingleton;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class jefeListDiferidos extends AppCompatActivity {
+import static java.security.AccessController.getContext;
+
+public class jefeListDiferidos extends AppCompatActivity implements Response.Listener<JSONObject>, Response.ErrorListener {
     ListView listadif;
     ConexionSQLiteHelper conec;
     ArrayList<String> liDife;
     ArrayList<Solicitud_Diferido> listadoDife;
-    String cod_oferta, codigo;
+    String cod_oferta,carnet_AlumDi,codigo;
+      JsonObjectRequest jsonObjectRequest;
+
 
 
     @Override
@@ -31,37 +47,42 @@ public class jefeListDiferidos extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jefe_list_diferidos);
 
-        ActionBar action=getSupportActionBar();
+        ActionBar action = getSupportActionBar();
         action.setDisplayHomeAsUpEnabled(true);
 
         listadif = (ListView) findViewById(R.id.listadif);
-        conec = new ConexionSQLiteHelper(getApplicationContext());
+        cargarWebService();
+       //conec = new ConexionSQLiteHelper(getApplicationContext());
 
         Bundle listav = getIntent().getExtras();
         codigo = listav.getString("jefecodmate");
 
-        consultarlistadiferidos();
+        //consultarlistadiferidos();
 
-        ArrayAdapter adaptador = new ArrayAdapter(this, android.R.layout.simple_list_item_1, liDife);
-        listadif.setAdapter(adaptador); //Adaptador
+        //ArrayAdapter adaptador = new ArrayAdapter(this, android.R.layout.simple_list_item_1, liDife);
+        //listadif.setAdapter(adaptador); //Adaptador
         listadif.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                cod_oferta= listadoDife.get(position).getId_solicitud().toString();
+                cod_oferta = listadoDife.get(position).getCarnet().toString();
+                carnet_AlumDi=listadoDife.get(position).getCarnet().toString();
 
                 Intent oferta = new Intent(jefeListDiferidos.this, jefeDiferido.class);
-                oferta.putExtra("jefecoddife",cod_oferta);
+                oferta.putExtra("jefecoddife", cod_oferta);
+                oferta.putExtra("carnetAlumDif",carnet_AlumDi);
                 startActivity(oferta);
 
 
             }
         });
 
+
     }
 
-    private void consultarlistadiferidos() {
-        SQLiteDatabase db = conec.getReadableDatabase();
+
+     //private void consultarlistadiferidos(){
+      /*  SQLiteDatabase db = conec.getReadableDatabase();
 
         Solicitud_Diferido materiaO;
 ///aqui modificar
@@ -74,18 +95,73 @@ public class jefeListDiferidos extends AppCompatActivity {
             materiaO.setCarnet(cursor.getString(1));
             materiaO.setIsss(cursor.getString(2));
 
-
             listadoDife.add(materiaO);
-        }
-        obtenerlista();
+}*/
+
+
+       // obtenerlista();
+
+
+    private void cargarWebService(){
+
+
+
+
+        String url="https://pdmconsumo.000webhostapp.com/consultarDiferidos.php";
+
+
+        jsonObjectRequest=new JsonObjectRequest(Request.Method.GET,url,null,this,this);
+        // request.add(jsonObjectRequest);
+        VolleySingleton.getIntanciaVolley(getApplicationContext()).addToRequestQueue(jsonObjectRequest);
+
+
     }
+
+
 
     private void obtenerlista() {
         liDife=new ArrayList<String>();
 
         for(int i=0; i<listadoDife.size();i++)
         {
-            liDife.add(listadoDife.get(i).getId_solicitud()+"   "+listadoDife.get(i).getCarnet());
+            liDife.add( " "+listadoDife.get(i).getCarnet());
         }
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+
+        Toast.makeText(getApplicationContext(),"No se pudo Consultar ",Toast.LENGTH_SHORT).show();
+        Log.i("ERROR",error.toString());
+    }
+
+    @Override
+    public void onResponse(JSONObject response) {
+        Solicitud_Diferido soliDife=null;
+        listadoDife= new ArrayList<>();
+
+        JSONArray json=response.optJSONArray("codigoCD");
+
+        try {
+
+            for (int i=0;i<json.length();i++){
+                soliDife=new Solicitud_Diferido();
+                JSONObject jsonObject=null;
+                jsonObject=json.getJSONObject(i);
+
+                soliDife.setCarnet(jsonObject.optString("carnet"));
+                listadoDife.add(soliDife);
+            }
+            obtenerlista();
+            ArrayAdapter adaptador = new ArrayAdapter(this, android.R.layout.simple_list_item_1, liDife);
+            listadif.setAdapter(adaptador);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(), "No se ha podido establecer conexión con el servidor" + " "+response, Toast.LENGTH_LONG).show();
+
+        }
+
+
     }
 }
